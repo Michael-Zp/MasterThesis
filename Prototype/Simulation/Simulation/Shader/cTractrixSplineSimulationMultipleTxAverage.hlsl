50275f25ecf02af7b80ef14010be5464e0467319
MichaelZp <michael.zappe@yahoo.de>
2020-07-29 00:39:55 +0200

    Hair style should now be more stable.

--- Prototype/Simulation/Simulation/Shader/cTractrixSplineSimulationMultipleTxAverage.hlsl
+++ Prototype/Simulation/Simulation/Shader/cTractrixSplineSimulationMultipleTxAverage.hlsl
@@ -688,8 +688,11 @@ void Simulation(uint3 DTid : SV_DispatchThreadID)
         desiredPosition[i] = strands[idx].Particles[i].Position + strands[idx].Particles[i].Velocity * deltaTime;
         
         float3 desiredPositionByHairStyle = strands[idx].Particles[i - 1].Position + normalize(strands[idx].DesiredSegmentDirections[i - 1]) * oldSegmentLength[i - 1];
-        float hairStyleFactor = 0.1;
-        strands[idx].Particles[i].Velocity += pow((desiredPositionByHairStyle - strands[idx].Particles[i].Position), 2) * 0.5;
+        float3 velocityToHairStylePos = desiredPositionByHairStyle - strands[idx].Particles[i].Position;
+        float hairStyleFactor = 0.5;
+        velocityToHairStylePos = sign(velocityToHairStylePos) * pow(velocityToHairStylePos, float3(2, 2, 2)) * hairStyleFactor;
+        
+        strands[idx].Particles[i].Velocity += velocityToHairStylePos;
     }
     
     //TODO Maybe use less space with using one 1D array, and calculating like:
